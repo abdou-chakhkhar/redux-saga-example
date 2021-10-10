467e92f246c71d19e9935d38b27efa80b5ee6803
abdou30.cia <abdou30.cia@gmail.com>
2021-10-10 11:09:42 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+
+import './App.css';
+import SagasExample from './components/saga-example/sagas-example.component';
+
+const App = props => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      hiho
+      <SagasExample />
     </div>
   );
-}
+};
 
 export default App;
